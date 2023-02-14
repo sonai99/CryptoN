78fa5be9f28b6c1fafb838444d4cc41e926e31cf
Anirban Dutta <anirbandutta428@gmail.com>
2023-02-14 22:09:14 +0530

    Update Coins.js

--- src/Coins/Coins.js
+++ src/Coins/Coins.js
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 import CoinItem from './CoinItem'
 import './Coins.css'
+import Coin from '../routes/Coin'
 
 const Coins = (props) => {
   return (
@@ -16,7 +18,9 @@ const Coins = (props) => {
                 </div>
                 {props.coins.map(coins=> {
                     return (
-                        <CoinItem coins={coins} key={coins.id}/>
+                        <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id} >
+                        <CoinItem coins={coins}/>
+                        </Link>
                     )
                 })}
 
